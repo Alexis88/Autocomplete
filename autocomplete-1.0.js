724dfe79687ca1d8d16134f599bd194bb73ec1e2
Alexis López Espinoza <Alexis88@users.noreply.github.com>
2015-05-20 00:22:06 -0500

    Update autocomplete-1.0.js

--- autocomplete-1.0.js
+++ autocomplete-1.0.js
@@ -78,10 +78,10 @@ var autocomplete = {
                 autocomplete.span.style.WebKitUserSelect = "none";
                 autocomplete.span.style.MozUserSelect = "none";
                 autocomplete.span.addEventListener("mouseover", function(){
-                	if ((autocomplete.aux = autocomplete.container.querySelectorAll("." + autocomplete.cls)).length){
+                if ((autocomplete.aux = autocomplete.container.querySelectorAll("." + autocomplete.cls)).length){
                        	autocomplete.aux[0].style.background = autocomplete.back || "lightyellow";
                        	autocomplete.aux[0].className = "";
-                   	}
+                }
                     this.style.background = autocomplete.hover || "gold";
                 }, false);
                 autocomplete.span.addEventListener("mouseout", function(){
