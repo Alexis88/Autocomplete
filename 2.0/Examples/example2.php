0e8a3e8fca4c5277fc7566f36dfe7c5d74dd6b8f
Alexis López Espinoza <Alexis88@users.noreply.github.com>
2015-06-27 12:29:05 -0500

    Update example2.php

--- 2.0/Examples/example2.php
+++ 2.0/Examples/example2.php
@@ -1,16 +1,16 @@
 <?php
-$connection = new mysqli('localhost', 'root', 'phpdeveloper', 'bd_prueba');
+$connection = new mysqli('server', 'user', 'password', 'bd_name');
 
 if ($connection->connect_error) exit ('Could not connect to the server');
 
 $term = $connection->real_escape_string(strip_tags(trim($_GET['term'])));
-$query = "SELECT * FROM ciudades WHERE nombre_ciudad LIKE '%{$term}%'";
+$query = "SELECT * FROM cities  WHERE city_name LIKE '%{$term}%'";
 $result = $connection->query($query) or exit ('Could not execute the query');
 $response = [];
 
 if ($result->num_rows){
 	while ($row = $result->fetch_array()){
-		$response[] = $row['nombre_ciudad'];
+		$response[] = $row['city_name'];
 	}
 	$result->free();
 }
