5d86cd3f76cd840185f028e39230c0ac6df035f9
Alexis López Espinoza <Alexis88@users.noreply.github.com>
2015-06-27 02:25:24 -0500

    Create example2.php

---
+++ 1.0/Examples/example2.php
@@ -0,0 +1,16 @@
+<?php
+$connection = new mysqli('server', 'user', 'password', 'bd_name');
+if ($connection->connect_error) exit ('Could not connect to the server');
+$term = $connection->real_escape_string(strip_tags(trim($_GET['term'])));
+$query = "SELECT city_name FROM cities WHERE city_name LIKE '%{$term}%'";
+$result = $connection->query($query) or exit ('Could not execute the query');
+$response = [];
+if ($result->num_rows){
+	while ($row = $result->fetch_array()){
+		$response[] = $row['city_name'];
+	}
+	$result->free();
+}
+$connection->close();
+echo json_encode($response);
+?>
