fe8647d5866a6bdee19bfc768d41a5581201cabe
Alexis López Espinoza <Alexis88@users.noreply.github.com>
2015-06-27 02:29:05 -0500

    Create example3.php

---
+++ 2.0/Examples/example3.php
@@ -0,0 +1,20 @@
+<?php
+$connection = new mysqli('localhost', 'root', 'phpdeveloper', 'bd_prueba');
+
+if ($connection->connect_error) exit ('Could not connect to the server');
+
+$term = $connection->real_escape_string(strip_tags(trim($_GET['term'])));
+$query = "SELECT * FROM clientes WHERE nombre LIKE '%{$term}%'";
+$result = $connection->query($query) or exit ('Could not execute the query');
+$response = [];
+
+if ($result->num_rows){
+	while ($row = $result->fetch_array()){
+		$response[] = $row['nombre'];
+	}
+	$result->free();
+}
+
+$connection->close();
+echo json_encode($response);
+?>
